a055f2978750e7267cace5cf507eaaa0f0ceb0ff
at-sacai <132660002+at-sacai@users.noreply.github.com>
2025-11-04 00:46:46 +0900

    Comment out error throw for missing image element
    Commented out the error throwing for missing image element.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function getImg() {
 	const img = document.getElementById('img');
 
 	if (!img) {
-		throw new Error("Element with id 'img' not found");
+		// throw new Error("Element with id 'img' not found");
 
 		const error = new Error("Element with id 'img' not found");
 		throw error;
