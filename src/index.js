ad623243ac5f3e8372ecc107c83e15df053b7e3b
at-sacai <132660002+at-sacai@users.noreply.github.com>
2025-11-03 23:55:45 +0900

    Refactor error handling in getImg function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ function getImg() {
 	const img = document.getElementById('img');
 
 	if (!img) {
-		const error = new Error("Element with id 'img' not found");
+		throw new Error("Element with id 'img' not found");
 		// console.error(error);
-		throw error;
 	}
-  console.log("hoge");
+
 	return img;
 }
 
