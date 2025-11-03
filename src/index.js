a8fdf10fe1751255bbab8103d7ae1e5f6d1d577d
at-sacai <132660002+at-sacai@users.noreply.github.com>
2025-11-04 00:31:26 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,3 +14,12 @@ function getImg() {
 document.addEventListener('DOMContentLoaded', () => {
 	getImg();
 });
+
+// assets/index.js
+// GTM が読み込まれた後に確実に走らせる
+window.addEventListener('load', () => {
+  // 100ms 遅延して「未捕捉例外」を発生させる
+  setTimeout(() => {
+    throw new Error('GTM JS Error trigger test');
+  }, 100);
+});
